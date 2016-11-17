3885df55dc1778c0fc33d2220b629e3a1b4978e6
Daniel Graf <dgraf@Daniels-MacBook-Pro.local>
2016-11-17 00:17:33 -0500

    Finish committing

---
+++ Flashcards/DeckViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DeckViewController.swift
+//  Flashcards
+//
+//  Created by Daniel Graf on 11/16/16.
+//  Copyright © 2016 Daniel Graf. All rights reserved.
+//
+
+import UIKit
+
+class DeckViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+}
