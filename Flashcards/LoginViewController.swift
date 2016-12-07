9ae71c4f34abc4485fa301cc434fa9041c084c19
Daniel Graf <dgraf@Daniels-MacBook-Pro.local>
2016-12-07 08:51:50 -0500

    Getting email works... Will have to change since it's optional in FB's schema

--- Flashcards/LoginViewController.swift
+++ Flashcards/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 import FacebookCore
 import FacebookLogin
 
@@ -19,6 +20,34 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
         }
         else {
             // Navigate to other view
+            checkFacebookEmail(accessToken: AccessToken.current!)
+            //works just commenting out
+//            let parameters:Parameters = ["user": [
+//                "id": 3,
+//                "email": "grafmark59@gmail.com"
+//                ]]
+//            let headers: HTTPHeaders = ["content-type": "application/json","accept": "application/json"]
+//            Alamofire.request("https://morning-castle-56124.herokuapp.com/users", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON(completionHandler: { response in
+//                var statusCode = response.response?.statusCode
+//                print(statusCode) //201 vs 500
+            
+                
+//                if (statusCode == 200){
+//                    self.setAuthToken(JSONData: response.data!)
+//                    UserDefaults.standard.set(email, forKey: "email")
+//                    UserDefaults.standard.set(password, forKey: "password")
+//                    let controller = ContainerViewController()
+//                    self.present(controller, animated: true, completion: nil)
+//                }
+//                else if (statusCode == 401){
+//                    // Create the alert box for failed log in
+//                    let alertController = UIAlertController(title: "Login Failed!", message: "Error: Should not see this text!", preferredStyle: UIAlertControllerStyle.alert)
+//                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+//                    alertController.message = "incorrect username or password"
+//                    self.present(alertController, animated: true, completion: nil)
+//                }
+            //})
+
             let mainController = ContainerViewController()
             present(mainController, animated: true, completion: nil)
         }
@@ -45,7 +74,7 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
             login(token: accessToken)
         }
         
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
         loginButton.center.x = view.center.x
         loginButton.center.y = view.center.y+200
         loginButton.delegate = self
@@ -59,6 +88,38 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name:NSNotification.Name.UIKeyboardWillShow, object: nil);
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name:NSNotification.Name.UIKeyboardWillHide, object: nil);
         
+        
+    }
+    
+    func checkFacebookEmail(accessToken: AccessToken) {
+        
+        GraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start { (urlResponse, requestResult) in
+            switch requestResult {
+            case .failed(let error):
+                print("error in graph request:", error)
+                break
+            case .success(let graphResponse):
+                if let responseDictionary = graphResponse.dictionaryValue {
+                    guard let email = responseDictionary["email"] else {
+                        print("Email error")
+                        break
+                    }
+                    self.checkIfUserExists(email: email as! String)
+                }
+            }
+        }
+
+    }
+    
+    func checkIfUserExists(email: String) {
+        //            let parameters:Parameters = ["user": [
+        //                "id": 3,
+        //                "email": "grafmark59@gmail.com"
+        //                ]]
+        //            let headers: HTTPHeaders = ["content-type": "application/json","accept": "application/json"]
+        //            Alamofire.request("https://morning-castle-56124.herokuapp.com/users", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON(completionHandler: { response in
+        //                var statusCode = response.response?.statusCode
+        //                print(statusCode) //201 vs 500
         
     }
     
